8e443e297139df526aa92a72babe8bd22a0bdb98
mahendramin <mahendramin19@gmail.com>
2023-06-05 18:36:55 +0700

    Add dummy data, create Employee Adapter and implement both of them in the MainActivity's RecyclerView

---
+++ app/src/main/java/com/example/dundermifflinemployee/EmployeeAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.dundermifflinemployee
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.dundermifflinemployee.databinding.ItemRowEmployeeBinding
+
+class EmployeeAdapter(private val listEmployee: ArrayList<Employee>) :
+    RecyclerView.Adapter<EmployeeAdapter.EmployeeViewHolder>() {
+    inner class EmployeeViewHolder(var binding: ItemRowEmployeeBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
+        val binding =
+            ItemRowEmployeeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return EmployeeViewHolder(binding)
+    }
+
+    override fun getItemCount() = listEmployee.size
+
+    override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
+        val name = listEmployee[position].name
+        val role = listEmployee[position].role
+        val image = listEmployee[position].image
+        holder.binding.apply {
+            tvEmployeeName.text = name
+            tvEmployeeRole.text = role
+            Glide.with(holder.itemView.context)
+                .load(image)
+                .into(imgEmployee)
+        }
+    }
+}
--- app/src/main/java/com/example/dundermifflinemployee/MainActivity.kt
+++ app/src/main/java/com/example/dundermifflinemployee/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.dundermifflinemployee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dundermifflinemployee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var listEmployee = ArrayList<Employee>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        listEmployee.addAll(getListEmployee())
+        setRecylerView()
+    }
+    private fun getListEmployee() : ArrayList<Employee> {
+        val listEmployee = ArrayList<Employee>()
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataRole = resources.getStringArray(R.array.data_role)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPicture = resources.obtainTypedArray(R.array.data_photo)
+        for (i in dataName.indices) {
+            val employee = Employee(dataName[i], dataRole[i], dataPicture.getResourceId(i, -1), dataDescription[i])
+            listEmployee.add(employee)
+        }
+        dataPicture.recycle()
+        return listEmployee
+    }
+
+    private fun setRecylerView() {
+        binding.rvEmployee.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val employeeAdapter = EmployeeAdapter(listEmployee)
+            adapter = employeeAdapter
+        }
     }
 }
